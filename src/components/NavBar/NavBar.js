09251e290ea4cfe3a32af3ebb9b23c77a8a93ee5
Eric Li <ericli@knights.ucf.edu>
2021-10-14 16:51:50 -0600

    Add initial nav bar css

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import './NavBar.scss';
 
 const NavBar = (props) => {
   return (
     <nav className="nav-bar">
-      <NavLink to="/">
+      <NavLink className="link" to="/">
         <h1 className="home-nav-text">Truck On</h1>
       </NavLink>
-      <NavLink to="/favorites">
+      <NavLink className="link" to="/favorites">
         <h1 className="favorite-nav-text">Favorites</h1>
       </NavLink>
     </nav>
