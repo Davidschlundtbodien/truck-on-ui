f1dc9fadec9c87e063d7c66e6c5326161ab30ac4
Eric Li <ericli@knights.ucf.edu>
2021-10-14 16:21:09 -0600

    Uncomment out sample test

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,7 +1,7 @@
-// import { render, screen } from '@testing-library/react';
-// import App from './App';
+import { render, screen } from '@testing-library/react';
+import App from './App';
 
-// test('renders learn react link', () => {
-//   render(<App />);
-//   // expect(true).to.equal(true);
-// });
+test('renders learn react link', () => {
+  render(<App />);
+  expect(true).to.equal(true);
+});
