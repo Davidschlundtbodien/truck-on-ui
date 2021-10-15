78a19856f7b34658747d6f603072d13c1db40cac
David Schlundt-Bodien <d.schlundtbodien@gmail.com>
2021-10-15 13:39:56 -0600

    Add trail details component

---
+++ src/components/TrailDetails/TrailDetails.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './TrailDetails.scss';
+
+const TrailDetails = () => {
+  return (
+    <article className="trail-details-container">
+      <p>Trail Details Here</p>
+    </article>
+  );
+}
+
+export default TrailDetails;
