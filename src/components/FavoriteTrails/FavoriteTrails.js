815f84e68c301118a337dfe4cfcc744663e9b671
David Schlundt-Bodien <d.schlundtbodien@gmail.com>
2021-10-15 13:57:07 -0600

    Add favorite trails component

---
+++ src/components/FavoriteTrails/FavoriteTrails.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './FavoriteTrails.scss';
+
+const FavoriteTrails = () => {
+  return (
+    <section className="favorite-trails-container">
+      <h1>Favorite Trails!</h1>
+    </section>
+  );
+}
+
+export default FavoriteTrails;
