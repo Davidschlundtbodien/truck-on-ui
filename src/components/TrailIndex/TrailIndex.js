d233d4fb59b0ec61657f8c760871108b436a209e
David Schlundt-Bodien <d.schlundtbodien@gmail.com>
2021-10-20 15:24:17 -0600

    Add query structure to trail index

--- src/components/TrailIndex/TrailIndex.js
+++ src/components/TrailIndex/TrailIndex.js
@@ -1,8 +1,15 @@
 import React from 'react';
 // import { Link } from 'react-router-dom';
 import './TrailIndex.scss';
+import { useQuery } from '@apollo/client';
+import { TRAIL_INDEX }from '../../graphql/queries'
 
 const TrailIndex = () => {
+  const {loading, error, data} = useQuery(TRAIL_INDEX)
+
+  if (loading) return 'Loading...';
+  if (error) return `Error! ${error.message}`;
+  
   return (
     <section className="trail-index-container">
       <h1>Trail Index/Home Test</h1>
