929bb1fddbc77a12b4d053e839c58c432b0102e0
David Schlundt-Bodien <d.schlundtbodien@gmail.com>
2021-10-21 14:25:58 -0600

    Add styling and restructure card component

--- src/components/TrailCard/TrailCard.js
+++ src/components/TrailCard/TrailCard.js
@@ -1,13 +1,28 @@
 import React from 'react';
+import './TrailCard.scss';
 import { Link } from 'react-router-dom'
 
 const TrailCard = ({ trail }) => {
+
+  const activities = trail.activities.map(activity => {
+    return (
+      <p>{activity}</p>
+    )
+  })
+
   return (
-    <Link to={`/trail/${trail.id}`} className="trail-card">
-      <h1>{trail.name}</h1>
-      <p>{trail.difficulty}</p>
-      <p>{trail.type}</p>
-      <p>{trail.activities}</p>
+    <Link to={`/trail/${trail.id}`} className="card-link">
+      <section className="trail-card">
+        <h1>{trail.name}</h1>
+        <p>Difficulty - {trail.difficulty}</p>
+        <p>Type - {trail.type}</p>
+        <article>
+          <p>Activities</p>
+          <div className="activities-list">
+            {activities}
+          </div>
+        </article>
+      </section>
     </Link>
   )
 }
