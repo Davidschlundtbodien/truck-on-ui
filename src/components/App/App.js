32a918db6572242fbc26ca86c741e0a24adc5b83
David Schlundt-Bodien <d.schlundtbodien@gmail.com>
2021-10-21 13:33:13 -0600

    Add list rendering with trail cards

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -13,7 +13,7 @@ const userLoggedIn = {id: 23, name: 'Eric'}
 
 const App = () => {
   const [allTrails, setAllTrails] = useState(null);
-  const [filteredTrails, setFilteredTrails] = useState(null);
+  const [filteredTrails, setFilteredTrails] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
 
   // On component mount, App should perform a fetch call to pull all trails data and setAllTrails
--- src/components/TrailIndex/TrailIndex.js
+++ src/components/TrailIndex/TrailIndex.js
@@ -3,16 +3,24 @@ import React from 'react';
 import './TrailIndex.scss';
 import { useQuery } from '@apollo/client';
 import { TRAIL_INDEX }from '../../graphql/queries'
+import TrailCard from '../TrailCard/TrailCard'
 
-const TrailIndex = () => {
+const TrailIndex = ({filteredTrails}) => {
   const {loading, error, data} = useQuery(TRAIL_INDEX)
 
   // if (loading) return 'Loading...';
   if (error) return `Error! ${error.message}`;
 
+  const trailList = filteredTrails.map(trail => {
+    return (
+      <TrailCard key={trail.id} trail={trail}/>
+    )
+  })
+
   return (
     <section className="trail-index-container">
       <h1>Trail Index/Home Test</h1>
+      {trailList}
     </section>
   );
 }
