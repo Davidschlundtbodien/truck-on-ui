ed942d2cd16c01aa442082f1599f84d5d2a74a39
David Schlundt-Bodien <d.schlundtbodien@gmail.com>
2021-10-21 14:00:01 -0600

    Add route link to trail card component

--- src/components/TrailCard/TrailCard.js
+++ src/components/TrailCard/TrailCard.js
@@ -3,9 +3,9 @@ import { Link } from 'react-router-dom'
 
 const TrailCard = ({ trail }) => {
   return (
-    <section className="trail-card">
+    <Link to={`/trail/${trail.id}`} className="trail-card">
       <h1>{trail.name}</h1>
-    </section>
+    </Link>
   )
 }
 
