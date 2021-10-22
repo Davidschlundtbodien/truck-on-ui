f25f9f4d7a329ff891a2d2da4d72b02a78594124
David Schlundt-Bodien <d.schlundtbodien@gmail.com>
2021-10-22 13:21:40 -0600

    Add gql query to trail index

--- src/components/TrailIndex/TrailIndex.js
+++ src/components/TrailIndex/TrailIndex.js
@@ -1,26 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './TrailIndex.scss';
 import { useQuery } from '@apollo/client';
 import { TRAIL_INDEX }from '../../graphql/queries'
 import TrailCard from '../TrailCard/TrailCard'
 
-const TrailIndex = ({filteredTrails}) => {
-  const {loading, error, data} = useQuery(TRAIL_INDEX)
+const TrailIndex = () => {
+  const [allTrails, setAllTrails] = useState([])
+  const {loading, error, data} = useQuery(TRAIL_INDEX);
 
-  // if (loading) return 'Loading...';
+  useEffect(() => {
+    if (data) {
+      setAllTrails(data.trails)
+    }
+  }, [data])
+
+  if (loading) return 'Loading...';
   if (error) return `Error! ${error.message}`;
 
-  const trailList = filteredTrails.map(trail => {
+
+  const trailList = allTrails.map(trail => {
     return (
       <TrailCard key={trail.id} trail={trail}/>
     )
   })
 
   return (
-    <section className="trail-index-container">
-      <h1>Trail Index/Home Test</h1>
-      {trailList}
-    </section>
+      <section className="trail-index-container">
+        <h1>Trail Index/Home Test</h1>
+        {trailList}
+      </section>
   );
 }
 
--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -1,12 +1,13 @@
 import { gql } from "@apollo/client";
 
 export const TRAIL_INDEX = gql`
-  query GetTrailIndex {
+  query trails {
     trails {
+      id
       name
       difficulty
       traffic
-      activities
+      routeType
     }
   }
 `;
@@ -14,10 +15,13 @@ export const TRAIL_INDEX = gql`
 export const SINGLE_TRAIL = gql`
   query GetSingleTrail($id: Integer!) {
     trail(id: $id) {
+      id
       name
+      description
+      elevationGain
+      nearestCity
       difficulty
       traffic
-      activities
     }
   }
 `;
