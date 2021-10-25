345cccd3ca62638a0f4871dfd6edebec7405dd94
Eric Li <ericli@knights.ucf.edu>
2021-10-25 16:34:49 -0600

    Add more images to imageList

---
+++ src/images/imageList.js
@@ -0,0 +1,29 @@
+import argentinePass from "./Argentine_Pass.jpeg";
+import blackBear from "./Black_Bear.jpeg";
+import boreasPass from "./Boreas_Pass.jpeg";
+import engineerPass from "./Engineer_Pass.jpeg";
+import mosquitoPass from "./Mosquito_Pass.jpeg";
+import redCone from "./Red_Cone.jpeg";
+import switzerland from "./Switzerland_Trail.jpeg";
+import chinnsLake from "./Chinns_Lake.jpeg";
+import hagermanPass from "./Hagerman_Pass.jpeg";
+import highWheel from "./High_Wheel.jpeg";
+import miniMoab from "./Mini_Moab.jpeg";
+import pearlPass from "./Pearl_Pass.jpeg";
+import imogenePass from "./Imogene_Pass.jpeg";
+
+export const imageList = [
+  argentinePass, 
+  blackBear, 
+  boreasPass, 
+  engineerPass, 
+  mosquitoPass, 
+  redCone, 
+  switzerland,
+  chinnsLake,
+  hagermanPass,
+  highWheel,
+  miniMoab,
+  pearlPass,
+  imogenePass
+]
