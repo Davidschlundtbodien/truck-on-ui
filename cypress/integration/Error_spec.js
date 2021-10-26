8a82a02a032403c2c3e298a009e9758ea9af0531
David Schlundt-Bodien <d.schlundtbodien@gmail.com>
2021-10-26 14:05:44 -0600

    Add Error handling test spec

---
+++ cypress/integration/Error_spec.js
@@ -0,0 +1,17 @@
+describe('Error handling', () => {
+  beforeEach(() => {
+    cy.interceptGraphQlQuery()
+    cy.visit('http://localhost:3000/incorrect')
+  })
+
+  it('should display a 404 page not found component for bad URL', () => {
+    cy.get('section')
+  })
+
+  it('should have a 404 page that directs the user back to the index', () =>{
+    cy.get('h1 > a')
+      .click()
+    cy.get('.trail-card-container >:nth-child(1)')
+      .contains('Mosquito Pass')
+  })
+})
