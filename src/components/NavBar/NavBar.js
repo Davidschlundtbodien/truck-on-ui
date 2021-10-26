60f359afb42642670052e6f3463ae144a9b24a4d
Eric Li <ericli@knights.ucf.edu>
2021-10-26 03:45:08 -0600

    Add activeStyles and media queries

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -5,10 +5,10 @@ import './NavBar.scss';
 const NavBar = ({ user }) => {
   return (
     <nav className="nav-bar">
-      <NavLink className="link" to="/">
+      <NavLink className="link" exact to="/" activeStyle={{filter: "brightness(0) invert(1)"}}>
         <h1 className="home-nav-text">Search Trails</h1>
       </NavLink>
-      <NavLink className="link" to={`/favorites/${user.id}`}>
+      <NavLink className="link" exact to={`/favorites/${user.id}`} activeStyle={{filter: "brightness(0) invert(1)"}}>
         <h1 className="favorite-nav-text">Favorites</h1>
       </NavLink>
     </nav>
