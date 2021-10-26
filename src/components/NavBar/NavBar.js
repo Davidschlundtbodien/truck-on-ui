16a75f984398cdd2f4bdc3a346b152db2353cf74
Eric Li <ericli@knights.ucf.edu>
2021-10-26 02:35:14 -0600

    Change NavBar inner text

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -6,7 +6,7 @@ const NavBar = ({ user }) => {
   return (
     <nav className="nav-bar">
       <NavLink className="link" to="/">
-        <h1 className="home-nav-text">Truck On {user.name}</h1>
+        <h1 className="home-nav-text">Search Trails</h1>
       </NavLink>
       <NavLink className="link" to={`/favorites/${user.id}`}>
         <h1 className="favorite-nav-text">Favorites</h1>
